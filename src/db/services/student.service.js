ba25efa5d3e78c3622857d7c10d35946bb2137b9
Oleh Kosylo <kosylopoker@gmail.com>
2020-06-25 19:07:07 +0300

    .gitignore / README.md

--- src/db/services/student.service.js
+++ src/db/services/student.service.js
@@ -2,16 +2,6 @@ const db = require('..').getInstance();
 const {modelsName: {STUDENT}} = require('../../config');
 
 module.exports = {
-    getStudentById: (id) => {
-        const StudentModel = db.getModel(STUDENT);
-
-        return StudentModel.findOne({
-            where: {id},
-            raw: true,
-            nest: true
-        })
-    },
-
     createStudent: async (student) => {
         const StudentModel = db.getModel(STUDENT);
 
